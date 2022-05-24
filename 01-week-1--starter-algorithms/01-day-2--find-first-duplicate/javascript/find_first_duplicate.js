bb6fbea465c0069a24072999eabd31261e150305
Henry Miller <mail.henrym+git@gmail.com>
2022-05-24 11:55:01 -0400

    find first duplicate

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,10 @@
 function findFirstDuplicate(arr) {
   // type your code here
+
+   for (let i= 0; i < arr.length; i++){
+     if (arr.indexOf(arr[i])!== i)return arr[i]
+   }
+   return -1
 }
 
 if (require.main === module) {
