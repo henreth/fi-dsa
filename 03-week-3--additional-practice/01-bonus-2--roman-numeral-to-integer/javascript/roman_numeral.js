62342f66ea611a8fea0f663b5caa1765f0cda434
Henry Miller <mail.henrym+git@gmail.com>
2022-05-24 23:53:23 -0400

    roman numeral

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,24 @@
 function romanNumeral(string) {
-  // type your code here
+  let symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
+  let count = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    // if symbol 1 has a value that is less than symbol 2
+    // that means it is supposed to lower their total value
+    // ie: IX -> 9 (I: 1, X:10)
+      symbols[s[i]] < symbols[s[i + 1]] ? count -= symbols[s[i]] : count += symbols[s[i]]
+  }
+
+  return count
+
 }
 
 if (require.main === module) {
