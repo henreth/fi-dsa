89c6fa3d73a1cf3a61c92b6d428e81144d85dc94
Henry Miller <mail.henrym+git@gmail.com>
2022-06-16 11:22:45 -0400

    binary tree balancing

--- 10-week-8--searching/02-day-5--build-a-binary-tree---balancing/javascript/balancing.js
+++ 10-week-8--searching/02-day-5--build-a-binary-tree---balancing/javascript/balancing.js
@@ -7,13 +7,33 @@ class Node {
 }
 
 // list = [3, 5, 6, 9, 10, 20]
+//        9
+//       / \
+//     5    10
+//    / \     \
+//   3   6     20
 function threeToTwenty() {
+  let three  = new Node(3)
+  let six = new Node(6)
+  let five =  new Node(5,three,six)
+  let twenty = new Node(20)
+  let ten = new Node(10,twenty)
+
+  return new Node(9,five,ten)
 
 }
 
 // list = [10, 11, 30, 100, 200]
+//           30
+//       11    100
+//     10        200
 function tenToTwoHundred() {
+  const ten = new Node(10);
+  const eleven = new Node(11, ten);
+  const two_hundred = new Node(200);
+  const hundred = new Node(100, null, two_hundred);
 
+  return new Node(30, eleven, hundred);
 }
 
 if (require.main === module) {
