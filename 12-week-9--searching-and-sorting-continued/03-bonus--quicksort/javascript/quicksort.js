b26b2e811279f5f0e4a8f91c2d2105b6b49ac7ea
Henry Miller <mail.henrym+git@gmail.com>
2022-06-16 12:59:54 -0400

    quick sort

--- 12-week-9--searching-and-sorting-continued/03-bonus--quicksort/javascript/quicksort.js
+++ 12-week-9--searching-and-sorting-continued/03-bonus--quicksort/javascript/quicksort.js
@@ -1,9 +1,40 @@
 function partition(array, low, high) {
-  // type your code in here
+  const pivot = array[high];  // choose rightmost element as pivot
+  let pivotIndex = high;  // store index of pivot
+
+  // iterate over the portion of the array that needs processing
+  // use a while loop to control the index, we only want to increment it
+  // if we encounter an element that's less than or equal to the pivot
+  let i = low;
+
+  while (i < pivotIndex) {
+    if (array[i] <= pivot) {
+      ++i;
+      continue;
+    }
+
+    const beforePivot = pivotIndex - 1;
+    // swap the higher valued element with the one just before the pivot
+    // then swap the element just before the pivot with the pivot
+    // then update the pivot index since it moved
+    [array[i], array[beforePivot]] = [array[beforePivot], array[i]];
+    [array[pivotIndex], array[beforePivot]] = [array[beforePivot], array[pivotIndex]];
+    --pivotIndex;
+  }
+
+  return pivotIndex;
 }
 
 function quicksort(array, low = 0, high = array.length - 1) {
-  // type your code here
+  if (low >= high) {
+    return array;
+  }
+
+  const pivotIndex = partition(array, low, high);
+  quicksort(array, low, pivotIndex - 1);
+  quicksort(array, pivotIndex + 1, high);
+
+  return array;
 }
 
 if (require.main === module) {
